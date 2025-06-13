b94f842325ece2a46cf355c05e93d992004a242d
resultfulness <alicja.rrosiak@gmail.com>
2025-06-13 10:28:47 +0200

    still working on ts

--- setup.sh
+++ setup.sh
@@ -3,11 +3,13 @@
 config_dir=$XDG_CONFIG_HOME/dotfiles
 home_config=$config_dir/home
 
+mkdir -p "$XDG_CONFIG_HOME"
 for config in `ls $home_config/xdg_config`; do
 	rm -f "$XDG_CONFIG_HOME/$config"
 	ln -s "$home_config/xdg_config/$config" "$XDG_CONFIG_HOME/$config"
 done
 
+mkdir -p ~/.local/bin
 for script in `ls $home_config/scripts`; do
 	ln -sf "$home_config/scripts/$script" ~/.local/bin/$script
 done
@@ -15,5 +17,6 @@ done
 ln -sf "$home_config/.bashrc" ~/.bashrc
 ln -sf "$home_config/.profile" ~/.profile
 
-rm ~/pictures/wallpapers
+mkdir -p ~/pictures
+rm -f ~/pictures/wallpapers
 ln -sf "$home_config/wallpapers" ~/pictures/wallpapers
