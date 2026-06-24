8fc736f83cf901e78460025735f80013fb8e007b
resultfulness <alicja.rrosiak@gmail.com>
2026-06-24 22:42:30 +0200

    some stuff

--- .config/nvim/lua/plugins/compile-mode.lua
+++ .config/nvim/lua/plugins/compile-mode.lua
@@ -1,15 +1,21 @@
 return {
     "ej-shafran/compile-mode.nvim",
     version = "^5.0.0",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim", "m00qek/baleia.nvim" },
     config = function()
+        vim.g.baleia = require("baleia").setup({})
+
         vim.g.compile_mode = {
             input_word_completion = true,
             bang_expansion = true,
+            baleia_setup = true,
             default_command = {
                 c = "cc -o %:r % && ./%:r",
                 rust = "cargo run",
                 python = "uv run main.py",
+                plaintex = "latexmk -pdflatex='lualatex -shell-escape -interaction nonstopmode' -pdf -f %",
+                tex = "latexmk -pdflatex='lualatex -shell-escape -interaction nonstopmode' -pdf -f %",
+                odin = "odin run ."
             },
         }
 
